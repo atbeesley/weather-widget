ce874556d97e0a37f283ef81d7923d4de58d4db9
atbeesley <alexander.beesley@gmail.com>
2021-02-06 11:51:25 +0000

    adds main component

---
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-widget-main',
+  templateUrl: './weather-widget-main.component.html',
+  styleUrls: ['./weather-widget-main.component.css']
+})
+export class WeatherWidgetMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
