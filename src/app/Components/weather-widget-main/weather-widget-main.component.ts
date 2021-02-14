1febca447e2ad8a00d999b9629fe7f444a49a711
atbeesley <alexander.beesley@gmail.com>
2021-02-14 20:06:05 +0000

    temp_celsius working okay

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -12,11 +12,11 @@ export class WeatherWidgetMainComponent implements OnInit {
 
   ngOnInit(): void {
     this.getWeatherData();
-    console.log(this.WeatherData);
+    // console.log(this.WeatherData);
   }
 
   getWeatherData(){
-    let data = JSON.parse('{"coord":{"lon":-3.4589,"lat":56.0716},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"stations","main":{"temp":274.32,"feels_like":266.67,"temp_min":273.15,"temp_max":275.93,"pressure":1018,"humidity":80},"visibility":10000,"wind":{"speed":7.72,"deg":80},"snow":{"1h":0.1},"clouds":{"all":90},"dt":1612697240,"sys":{"type":1,"id":1442,"country":"GB","sunrise":1612684611,"sunset":1612717150},"timezone":0,"id":2650732,"name":"Dunfermline","cod":200}');
+    let data = JSON.parse('{"coord":{"lon":-3.4589,"lat":56.0716},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":279.18,"feels_like":272.46,"temp_min":278.71,"temp_max":280.15,"pressure":1005,"humidity":87},"visibility":10000,"wind":{"speed":7.72,"deg":220,"gust":12.86},"clouds":{"all":75},"dt":1613332610,"sys":{"type":1,"id":1442,"country":"GB","sunrise":1613288495,"sunset":1613322881},"timezone":0,"id":2650732,"name":"Dunfermline","cod":200}');
     this.setWeatherData(data);
   }
 
@@ -25,16 +25,23 @@ export class WeatherWidgetMainComponent implements OnInit {
 
     let sunsetTime = new Date(this.WeatherData.sys.sunset*1000);
     this.WeatherData.sunset_time = sunsetTime.toLocaleString();
-    console.log(sunsetTime);
+    // console.log(this.WeatherData.sunset_time);
 
     let sunriseTime = new Date(this.WeatherData.sys.sunrise*1000);
     this.WeatherData.sunrise_time = sunriseTime.toLocaleString();
-    console.log(sunriseTime);
+    // console.log(sunriseTime);
 
     let currentDate = new Date();
-    this.WeatherData.isDaytime = (currentDate.toLocaleTimeString() < sunsetTime.toLocaleTimeString());
-    console.log("is it daytime? " + this.WeatherData.isDaytime);
-    
+    this.WeatherData.isDay = (currentDate.toLocaleTimeString() < sunsetTime.toLocaleTimeString());
+    // console.log("is it daytime? " + this.WeatherData.isDay);
+
+    this.WeatherData.temp_celsius = (this.WeatherData.main.temp- 273.15).toFixed(0);
+    console.log(this.WeatherData.temp_celsius);
+
+
+
+    // this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_Like - 273.15).toFixed(0);
+    // console.log(this.WeatherData.temp_feels_like);
   }
 
 }
